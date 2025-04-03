33afea8c2a04e6a775317db5c3b8405d70f03c8f
Kirill <k187m18733@gmail.com>
2025-04-03 16:24:50 +0300

    done carlights anim

--- src/js/files/gsap/hero/hero.js
+++ src/js/files/gsap/hero/hero.js
@@ -1,10 +1,19 @@
 import gsap from "gsap";
 
 document.addEventListener("DOMContentLoaded", () => {
-    const tl = gsap.timeline();
+  const tl = gsap.timeline();
 
-    tl.from('.hero__title', {y: 30, autoAlpha: 0, duration: 0.5, ease: "power1.out"}, '+=0.1')
-    .from('.hero__text', {y: 30, autoAlpha: 0, duration: 0.5, ease: "power1.out"})
-    .from('.hero__slogan', {y: 30, autoAlpha: 0, duration: 0.5, ease: "power1.out"}, '-=0.1')
-    .from('.hero__button', {y: 30, autoAlpha: 0, duration: 0.5, ease: "power1.out" }, '+=0.2')
+  tl.from('.hero__title', { y: 30, autoAlpha: 0, duration: 0.5, ease: "power1.out" }, '+=0.1')
+    .from('.hero__text', { y: 30, autoAlpha: 0, duration: 0.5, ease: "power1.out" })
+    .from('.hero__slogan', { y: 30, autoAlpha: 0, duration: 0.5, ease: "power1.out" }, '-=0.1')
+    .from('.hero__button', { y: 30, autoAlpha: 0, duration: 0.5, ease: "power1.out" }, '+=0.2')
+    .fromTo('.carlights',
+      { autoAlpha: 0 }, // Начальное состояние (невидимы)
+      {
+        autoAlpha: 1,   // Появляются
+        duration: 0.5,
+        ease: "power1.out",
+        yoyo: true,     // Автоматически возвращается к начальному состоянию
+        repeat: 1,      // Повторяем 1 раз (появится и исчезнет)
+      })
 })
