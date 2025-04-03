1399a7932f6d00518495429257ae9099199d8923
Kirill <k187m18733@gmail.com>
2025-04-03 16:25:55 +0300

    rename carlight to headlights

--- src/js/files/gsap/hero/hero.js
+++ src/js/files/gsap/hero/hero.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
     .from('.hero__text', { y: 30, autoAlpha: 0, duration: 0.5, ease: "power1.out" })
     .from('.hero__slogan', { y: 30, autoAlpha: 0, duration: 0.5, ease: "power1.out" }, '-=0.1')
     .from('.hero__button', { y: 30, autoAlpha: 0, duration: 0.5, ease: "power1.out" }, '+=0.2')
-    .fromTo('.carlights',
+    .fromTo('.headlights',
       { autoAlpha: 0 }, // Начальное состояние (невидимы)
       {
         autoAlpha: 1,   // Появляются
