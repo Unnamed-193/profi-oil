ebe135a859cbec8dc65ac2b3beeadd87922db161
Kirill <k187m18733@gmail.com>
2025-04-03 13:31:00 +0300

    done easystart section and animation

--- src/js/files/gsap/hero/hero.js
+++ src/js/files/gsap/hero/hero.js
@@ -6,5 +6,5 @@ document.addEventListener("DOMContentLoaded", () => {
     tl.from('.hero__title', {y: 30, autoAlpha: 0, duration: 0.5, ease: "power1.out"}, '+=0.1')
     .from('.hero__text', {y: 30, autoAlpha: 0, duration: 0.5, ease: "power1.out"})
     .from('.hero__slogan', {y: 30, autoAlpha: 0, duration: 0.5, ease: "power1.out"}, '-=0.1')
-    .from('.hero__button', {y: 30, autoAlpha: 0, duration: 0.5, ease: "power1.out"}, '+=0.2')
+    .from('.hero__button', {y: 30, autoAlpha: 0, duration: 0.5, ease: "power1.out" }, '+=0.2')
 })
