16ab1919a86ebb47e1480bba0d72f73449802ce4
Kirill <k187m18733@gmail.com>
2025-04-04 16:34:47 +0300

    refactor hero.js

--- src/js/files/gsap/hero/hero.js
+++ src/js/files/gsap/hero/hero.js
@@ -1,21 +1,17 @@
 import gsap from "gsap";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const tl = gsap.timeline();
+  const tl = gsap.timeline({ delay: 0.2, defaults: { duration: 0.5, ease: "power1.out" } });
   const isMobile = window.matchMedia("(max-width: 767px)").matches;
 
   // 1. Анимация заголовка и текста
   tl.from('.hero__title', { 
     y: 30, 
     autoAlpha: 0, 
-    duration: 0.5, 
-    ease: "power1.out" 
   }, '+=0.1')
     .from('.hero__text', { 
       y: 30, 
       autoAlpha: 0, 
-      duration: 0.5, 
-      ease: "power1.out" 
     });
 
   // 2. Анимация slogan-box (после текста, перед кнопкой)
@@ -23,15 +19,11 @@ document.addEventListener("DOMContentLoaded", () => {
     tl.from(".hero__slogan-box", {
       clipPath: "inset(0 0 0 100%)", // Слева направо
       autoAlpha: 0,
-      duration: 0.5,
-      ease: "power1.out"
     }, "-=0.1"); // Начинается чуть раньше завершения анимации текста
   } else {
     tl.from('.hero__slogan-box', { 
       y: 30, 
       autoAlpha: 0, 
-      duration: 0.5, 
-      ease: "power1.out" 
     }, "-=0.1");
   }
 
@@ -39,8 +31,6 @@ document.addEventListener("DOMContentLoaded", () => {
   tl.from('.hero__button', { 
     y: 30, 
     autoAlpha: 0, 
-    duration: 0.5, 
-    ease: "power1.out" 
   }, "+=0.2"); // Задержка после slogan-box
 
   // 4. Анимация фар (после всех элементов)
@@ -48,8 +38,6 @@ document.addEventListener("DOMContentLoaded", () => {
     { autoAlpha: 0 }, 
     { 
       autoAlpha: 1, 
-      duration: 0.5, 
-      ease: "power1.out", 
       yoyo: true, 
       repeat: 1 
     }
