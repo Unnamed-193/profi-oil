b5b6a3c63d3b1ece499afd88f44eea793925aeac
Kirill <k187m18733@gmail.com>
2025-04-04 14:36:38 +0300

    done car adaptive

--- src/js/files/gsap/hero/hero.js
+++ src/js/files/gsap/hero/hero.js
@@ -7,13 +7,13 @@ document.addEventListener("DOMContentLoaded", () => {
     .from('.hero__text', { y: 30, autoAlpha: 0, duration: 0.5, ease: "power1.out" })
     .from('.hero__slogan', { y: 30, autoAlpha: 0, duration: 0.5, ease: "power1.out" }, '-=0.1')
     .from('.hero__button', { y: 30, autoAlpha: 0, duration: 0.5, ease: "power1.out" }, '+=0.2')
-    // .fromTo('.headlights',
-    //   { autoAlpha: 0 }, // Начальное состояние (невидимы)
-    //   {
-    //     autoAlpha: 1,   // Появляются
-    //     duration: 0.5,
-    //     ease: "power1.out",
-    //     yoyo: true,     // Автоматически возвращается к начальному состоянию
-    //     repeat: 1,      // Повторяем 1 раз (появится и исчезнет)
-    //   })
+    .fromTo('.headlights',
+      { autoAlpha: 0 }, // Начальное состояние (невидимы)
+      {
+        autoAlpha: 1,   // Появляются
+        duration: 0.5,
+        ease: "power1.out",
+        yoyo: true,     // Автоматически возвращается к начальному состоянию
+        repeat: 1,      // Повторяем 1 раз (появится и исчезнет)
+      })
 })
