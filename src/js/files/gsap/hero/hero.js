6d048e857fb5a27d7a1f73683761fcef69726e6a
Kirill <k187m18733@gmail.com>
2025-04-30 14:33:38 +0300

    fix anim

--- src/js/files/gsap/hero/hero.js
+++ src/js/files/gsap/hero/hero.js
@@ -1,18 +1,21 @@
 import gsap from "gsap";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const tl = gsap.timeline({ delay: 0.2, defaults: { duration: 0.3, ease: "power1.out" } });
+  const tl = gsap.timeline({ delay: 0.2, defaults: { duration: 0.4, ease: "power1.in" } });
   const isMobile = window.matchMedia("(max-width: 767px)").matches;
 
   // 1. Анимация заголовка и текста
   tl.from('.hero__title', { 
     y: 30, 
     autoAlpha: 0, 
-  }, '+=0.1')
-    .from('.hero__text', { 
+    ease: "none"
+  }, 'text')
+    tl.from('.hero__text', { 
+      delay: 0.09,
       y: 30, 
       autoAlpha: 0, 
-    });
+      ease: "sine.in"
+    }, 'text');
 
   // 2. Анимация slogan-box (после текста, перед кнопкой)
   if (isMobile) {
